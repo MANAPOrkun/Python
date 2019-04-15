d2806a69c0d1590dd71b37c33e154c4586b67bfa
Orkun Manap <31966136+MANAPOrkun@users.noreply.github.com>
2019-04-15 20:22:33 +0300

    Delete asal sayı.py

--- asal sayı.py
+++
@@ -1,9 +0,0 @@
-sayi=int(input("Sayi giriniz: "))
-liste=[*range(2,sayi)]
-for i in liste:
-    if(sayi%i==0):
-        print(sayi,"Asal sayı değildir.")
-        break
-    elif sayi%i!=0:
-        print("Asal sayıdır.")
-        break
