16e0741d3ab63254e3b65852d887f2ef3bc3143b
Orkun Manap <31966136+MANAPOrkun@users.noreply.github.com>
2019-04-15 20:22:53 +0300

    Delete kullanıcı değerleri toplama.py

--- kullanıcı değerleri toplama.py
+++
@@ -1,9 +0,0 @@
-toplam=0
-while True:
-    deger=(input("Lütfen değer giriniz: "))
-    if deger=="q":
-        print("Programdan çıkılıyor...")
-        break
-    deger=int(deger)
-    toplam+=deger
-print("Toplam: ", toplam)
