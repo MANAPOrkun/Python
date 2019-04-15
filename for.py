e768fa32499e44895b2310ee86f41e2cefed3e92
Orkun Manap <31966136+MANAPOrkun@users.noreply.github.com>
2019-04-15 20:22:50 +0300

    Delete for.py

--- for.py
+++
@@ -1,8 +0,0 @@
-sayı=tuple(input("Lütfen sayı değeri giriniz: "))
-
-for a in sayı:
-    a = int(a)
-    if a % 2 == 0:
-        print("Girdiğiniz şu değer çifttir:",a)
-    else:
-        print("Girdiğiniz şu değer tektir:",a)
