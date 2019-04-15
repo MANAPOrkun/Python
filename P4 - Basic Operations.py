86d788b310e3059eeffd2a547e449bd88adf7034
Orkun Manap <31966136+MANAPOrkun@users.noreply.github.com>
2019-04-15 20:25:08 +0300

    Add files via upload

---
+++ P4 - Basic Operations.py
@@ -0,0 +1,12 @@
+numb1=5
+numb2=4
+
+print("Initial values: ",numb1,numb2)
+
+print("Summation: ",numb1+numb2)
+
+print("Multiplication: ",numb1*numb2)
+
+print("Division: ",numb1/numb2)
+
+print("Subtraction: ",numb1-numb2)
---
+++ P5 - Swap Values.py
@@ -0,0 +1,6 @@
+a=int(input("Enter first integer: "))
+b=int(input("Enter second integer: "))
+
+print("Before Swapping: ",a,b)
+a,b=b,a
+print("After Swapping: ",a,b)
