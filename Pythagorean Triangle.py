de6f8e293e96badf92869d58987123fc02cd98a8
Orkun Manap <manaporkun@gmail.com>
2019-06-18 21:55:33 +0300

    Delete Pythagorean Triangle.py

--- Pythagorean Triangle.py
+++
@@ -1,9 +0,0 @@
-def PTriangle(number):
-
-    for i in range(1,number):
-        for j in range(i,number):
-            k = (i**2+j**2)**0.5
-            if(k % 1 == 0):
-                print (i,j,k)
-
-PTriangle(100)
