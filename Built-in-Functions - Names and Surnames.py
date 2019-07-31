2e02d92c1d709210d51c39f491df7722a277c1bd
Orkun Manap <manaporkun@gmail.com>
2019-07-31 15:13:05 +0300

    Update Built-in-Functions - Names and Surnames.py

--- Built-in-Functions - Names and Surnames.py
+++ Built-in-Functions - Names and Surnames.py
@@ -1,6 +1,10 @@
 names = ["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
 surnames = ["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
 
-for person in list(zip(names,surnames)):
+people = list(zip(names,surnames))
+
+people.sort()
+
+for person in people:
 
     print(person[0],person[1])
